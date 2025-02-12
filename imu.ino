1db598cfdf641783a4614e3d16d3864789ba264b
Gustavo Nunes <109112700+guxtaavo@users.noreply.github.com>
2025-02-12 15:29:33 -0300

    Update imu.ino

--- imu.ino
+++ imu.ino
@@ -1,8 +1,9 @@
-#include "Arduino_BMI270_BMM150.h"
+#include "Arduino_BMI270_BMM150.h"  // Biblioteca para o sensor BMI270 e BMM150
 
 float accX, accY, accZ;
 float gyroX, gyroY, gyroZ;
-float degreesX = 0, degreesY = 0, degreesZ = 0;
+float mx, my, mz; 
+float roll, pitch, yaw; 
 unsigned long lastTime;
 
 void setup() {
@@ -15,23 +16,24 @@ void setup() {
     while (1);
   }
 
-  lastTime = millis(); // Inicializa o tempo
+  lastTime = millis();
 }
 
 void loop() {
   unsigned long currentTime = millis();
-  float deltaTime = (currentTime - lastTime) / 1000.0; // Converte para segundos
+  float deltaTime = (currentTime - lastTime) / 1000.0; // segundos
   lastTime = currentTime;
 
-  if (IMU.accelerationAvailable() && IMU.gyroscopeAvailable()) {
+  if (IMU.accelerationAvailable() && IMU.gyroscopeAvailable() && IMU.magneticFieldAvailable()) {
     IMU.readAcceleration(accX, accY, accZ);
     IMU.readGyroscope(gyroX, gyroY, gyroZ);
+    IMU.readMagneticField(mx, my, mz);
+    roll = atan2(accY, accZ) * 180 / PI;
+    pitch = -atan2(-accX, sqrt(accY * accY + accZ * accZ)) * 180 / PI;
+    yaw +=  gyroZ * deltaTime;
 
-    // Roll e Pitch pelo acelerômetro (faixa de -180° a 180°)
-    float roll = atan2(accY, sqrt(accX * accX + accZ * accZ)) * 180 / PI;
-    float pitch = atan2(accX, sqrt(accY * accY + accZ * accZ)) * 180 / PI;
-    float yaw = gyroZ; // Tem que corrigir
 
+    // Exibe os valores de Roll, Pitch e Yaw
     Serial.print("Roll (X): "); Serial.print(roll); Serial.print("° ");
     Serial.print("Pitch (Y): "); Serial.print(pitch); Serial.print("° ");
     Serial.print("Yaw (Z): "); Serial.print(yaw); Serial.println("°");
